1dfa38dab7a8e5f5abc7b5c7a615d1270e0e7b68
Alok Gupta <alok.gupta@pdgc.com>
2022-01-13 19:36:07 +0530

    Added upsert functionality

--- SupplyWebApp/Controllers/FileUploadController.cs
+++ SupplyWebApp/Controllers/FileUploadController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using SupplyWebApp.Data;
 using SupplyWebApp.Models;
 using System;
+using System.Linq;
 using System.Data;
 using System.IO;
 using System.Net.Http.Headers;
@@ -17,7 +18,6 @@ namespace SupplyWebApp.Controllers
     {
         private IHostingEnvironment _hostingEnvironment;
         private DataContext _dataContext;
-
         public FileUploadController(IHostingEnvironment hostingEnvironment, DataContext context)
         {
             _hostingEnvironment = hostingEnvironment;
@@ -74,11 +74,12 @@ namespace SupplyWebApp.Controllers
                             if (dataSetFromExcel != null && dataSetFromExcel.Tables.Count > 0)
                             {
                                 DataTable dtSalesForecast = dataSetFromExcel.Tables[0];
-                                
+
                                 SalesForecast salesForecast;
 
                                 for (int i = 2; i < dtSalesForecast.Rows.Count; i++)
                                 {
+
                                     salesForecast = new SalesForecast
                                     {
                                         Year = Convert.ToInt32(dtSalesForecast.Rows[i][0]),
@@ -87,7 +88,19 @@ namespace SupplyWebApp.Controllers
                                         Amount = Convert.ToInt64(dtSalesForecast.Rows[i][3])
                                     };
 
-                                    _dataContext.SalesForecast.Add(salesForecast);
+                                    var salesForecastFromDatabase = _dataContext.SalesForecast
+                                        .Where(sf => sf.Year == salesForecast.Year
+                                        && sf.Month == salesForecast.Month
+                                        && sf.Location.Equals(salesForecast.Location)).FirstOrDefault();
+
+                                    if (salesForecastFromDatabase != null)
+                                    {
+                                        salesForecastFromDatabase.Amount = salesForecast.Amount;
+                                    }
+                                    else
+                                    {
+                                        _dataContext.SalesForecast.Add(salesForecast);
+                                    }
                                 }
 
                                 int output = _dataContext.SaveChanges();
@@ -104,11 +117,13 @@ namespace SupplyWebApp.Controllers
                             else
                             {
                                 message = "Selected file is empty.";
+                                //return HttpContext.Response.StatusCode = 500 ;
                             }
                         }
                         else
                         {
                             message = "Invalid File.";
+                            return BadRequest();
                         }
                     }
                 }

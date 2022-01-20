c3c866883a81210a40b99156e5fe349cceac2902
Alok Gupta <alok.gupta@pdgc.com>
2022-01-20 20:08:22 +0530

    Fixes to CSS

--- SupplyWebApp/ClientApp/src/app/upload-file/upload-file.component.ts
+++ SupplyWebApp/ClientApp/src/app/upload-file/upload-file.component.ts
@@ -95,10 +95,8 @@ export class UploadFileComponent implements OnInit {
     let name = target.files[0].name;
     let allowedExtensions = /(\.xls|\.xlsx)$/i;
     if (!allowedExtensions.exec(name)) {
-      //alert('Please select an Excel File');
       this.toastr.error('Please select an Excel File');
       file.value = '';
-      //return false;
     }
     const reader: FileReader = new FileReader();
 
@@ -116,7 +114,6 @@ export class UploadFileComponent implements OnInit {
 
       this.data = this.sheet.slice(headerEnd);
       this.header = this.sheet.slice(0, headerEnd);
-      //console.log(x);
 
     };
 

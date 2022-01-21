dc8438346b77ca57eb30c30f974e090ce55bde7a
Priyanka Ghonge <priyanka.ghonge@pdgc.com>
2022-01-21 15:23:05 +0530

    Fixed issue that file should be selected according to the file type

--- SupplyWebApp/ClientApp/src/app/upload-file/upload-file.component.ts
+++ SupplyWebApp/ClientApp/src/app/upload-file/upload-file.component.ts
@@ -16,6 +16,7 @@ export class UploadFileComponent implements OnInit {
   data: [][];
   x: [][];
   baseUrl: string;
+  fileName: string;
   @ViewChild('labelImport', { static: true })
   @ViewChild('file', { static: false })
   InputVar: ElementRef;
@@ -25,7 +26,7 @@ export class UploadFileComponent implements OnInit {
   display: boolean = false;
   to: string = '2022-01-01'; 
   from: string = '2022-01-01';
-
+  
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private toastr: ToastrService) {
     this.baseUrl = baseUrl;
   }
@@ -95,15 +96,19 @@ export class UploadFileComponent implements OnInit {
     if (target.files.length !== 1) {
       this.toastr.error('Cannot upload multiple files');
     }
-    let name = target.files[0].name;
+    this.fileName = target.files[0].name;
     let allowedExtensions = /(\.xls|\.xlsx)$/i;
-    if (!allowedExtensions.exec(name)) {
+    if (!allowedExtensions.exec(this.fileName)) {
       this.toastr.error('Please select an Excel File');
       file.value = '';
     }
     const reader: FileReader = new FileReader();
 
     reader.onload = (e: any) => {
+      let fileColumnHeader: string[][] = [['Year', 'Month', 'Location', 'Amount'], ['Spot prices', 'WEEK 1', 'WEEK 2', 'WEEK 3', 'WEEK 4', 'WEEK 5'], ['Year', 'Month', 'Location', 'Amount']];
+      let isValidFile: boolean
+      
+
       const bstr: string = e.target.result;
 
       const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
@@ -118,6 +123,38 @@ export class UploadFileComponent implements OnInit {
       this.data = this.sheet.slice(headerEnd);
       this.header = this.sheet.slice(0, headerEnd);
 
+      if (this.typeOfFile == "F_01" && this.fileName.indexOf("Sales Forecast") > 0) {
+        for (var i = 0; i < 3; i++) {
+          if (this.header[0][i] == fileColumnHeader[0][i])
+            isValidFile = true;
+          else
+            isValidFile = false
+        }
+      }
+      else if (this.typeOfFile == "F_02" && this.fileName.indexOf("CRU Pricing") > 0) {
+        for (var i = 0; i < 5; i++) {
+          if (this.header[0][i] == fileColumnHeader[1][i])
+            isValidFile = true;
+          else
+            isValidFile = false
+        }
+      }
+      else if (this.typeOfFile == "F_03" && this.fileName.indexOf("Planned Buy") > 0) {
+        for (var i = 0; i < 3; i++) {
+          if (this.header[0][i] == fileColumnHeader[2][i])
+            isValidFile = true;
+          else
+            isValidFile = false
+        }
+      }
+
+      if (!isValidFile) {
+        this.toastr.error('Please select appropriate file for File Type');
+        this.reset();
+      }
+      
+      console.log(isValidFile);
+      
     };
 
     reader.readAsBinaryString(target.files[0]);

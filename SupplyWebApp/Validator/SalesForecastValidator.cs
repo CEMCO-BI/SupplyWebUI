f17371e2839fdca58aebf5bcf8f4032dbd7a494c
JibranKhan <jibran.khan@pdgc.com>
2022-02-08 12:11:40 +0530

    amount validation

--- SupplyWebApp/Validator/SalesForecastValidator.cs
+++ SupplyWebApp/Validator/SalesForecastValidator.cs
@@ -10,48 +10,43 @@ using static SupplyWebApp.Helpers.Enums;
 using Microsoft.Extensions.Configuration;
 using ExcelDataReader;
 using SupplyWebApp.Models;
+using System.Text.RegularExpressions;
 
 
 
 
-    public class SalesForecastValidator : AbstractValidator<SalesForecast>
+public class SalesForecastValidator : AbstractValidator<SalesForecast>
+{
+    public SalesForecastValidator()
     {
-        public SalesForecastValidator()
+        RuleFor(sf => sf.Year.ToString().Length)
+        .Cascade(CascadeMode.Continue)
+        .Equal(4).WithMessage("Please enter a valid Year.");
+
+        RuleFor(sf => sf.Month)
+        .Cascade(CascadeMode.Continue)
+        .Must(isAValidMonth)
+        .WithMessage("Please enter a valid Month.");
+
+        RuleFor(sf => sf.Location)
+        .Cascade(CascadeMode.Continue)
+        .Must(isAValidLocation).WithMessage("Please enter a valid Location.");
+
+        RuleFor(sf => sf.Amount.ToString())
+        .Cascade(CascadeMode.Continue)
+        .Must(isAValidAmount).WithMessage("Please enter a numeric value for Amount.");
+    }
+
+    public bool isAValidMonth(int month)
+    {
+        for (int i = 1; i <= 12; i++)
         {
-            RuleFor(sf => sf.Year.ToString().Length)
-            .Cascade(CascadeMode.Continue)
-            .Equal(4).WithMessage("Please enter a valid year.");
+            if (month == i)
+                return true;
 
-            RuleFor(sf => sf.Month)
-            .Cascade(CascadeMode.Continue)
-            .Must(isAValidMonth)
-            .WithMessage("Please enter a valid month.");
-
-            RuleFor(sf => sf.Location)
-            .Cascade(CascadeMode.Continue)
-            .Must(isAValidLocation).WithMessage("Please enter a valid location.");
         }
-
-        public bool isAValidMonth(int month)
-        {
-            for (int i = 1; i <= 12; i++)
-            {
-                if (month == i)
-                    return true;
-
-            }
-            return false;
-        }
-        public bool isAValidLocation(string location)
-        {
-            String[] s = { "IND", "DEN", "PIT", "FTW" };
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (location == s[i])
-                    return true;
-            }
-            return false;
-        }
+        return false;
+    }
     public bool isAValidLocation(string location)
     {
         String[] s = { "IND", "DEN", "PIT", "FTW" };
@@ -61,5 +56,12 @@ using SupplyWebApp.Models;
                 return true;
         }
         return false;
+    }
+
+    public bool isAValidAmount(string amount)
+    {
+        var regex = new Regex("[+-]?([0-9]*[.])?[0-9]+");
+
+        return regex.IsMatch(amount);
     }
 }

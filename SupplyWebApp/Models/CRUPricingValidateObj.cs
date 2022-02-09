ab87eb87fbef5c26918813dacfdab093d5d70b77
JibranKhan <jibran.khan@pdgc.com>
2022-02-09 20:42:08 +0530

    cru validator created.

---
+++ SupplyWebApp/Models/CRUPricingValidateObj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupplyWebApp.Models
+{
+    public class CRUPricingValidateObj
+    {
+        public string date_v { get; set; }
+        public string Week1_v { get; set; }
+        public string Week2_v { get; set; }
+        public string Week3_v { get; set; }
+        public string Week4_v { get; set; }
+        public string Week5_v { get; set; }
+
+
+        public CRUPricingValidateObj( string v2, string v3, string v4, string v5, string v6, string v7)
+        {
+            this.date_v = v2;
+            this.Week1_v = v3;
+            this.Week2_v = v4;
+            this.Week3_v = v5;
+            this.Week4_v = v6;
+            this.Week5_v = v7;
+        }
+    }
+}
---
+++ SupplyWebApp/Validator/CRUPricingValidator.cs
@@ -0,0 +1,86 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SupplyWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static SupplyWebApp.Helpers.Enums;
+using Microsoft.Extensions.Configuration;
+using ExcelDataReader;
+using SupplyWebApp.Models;
+using System.Text.RegularExpressions;
+
+
+    public class CRUPricingValidator : AbstractValidator<CRUPricingValidateObj>
+    {
+        public CRUPricingValidator() {
+
+        RuleFor(cru => cru.date_v)
+       .Cascade(CascadeMode.Continue)
+       .Must(IsValidYear)
+       .WithMessage("Please enter a valid Year.");
+
+        RuleFor(cru => cru.date_v)
+       .Cascade(CascadeMode.Continue)
+       .Must(IsAValidMonth)
+       .WithMessage("Please enter a valid Month.");
+
+        RuleFor(cru => cru.Week1_v)
+       .Cascade(CascadeMode.Continue)
+       .Must(IsAValidAmount).WithMessage("Please enter a numeric value for Amount.");
+
+        RuleFor(cru => cru.Week2_v)
+      .Cascade(CascadeMode.Continue)
+      .Must(IsAValidAmount).WithMessage("Please enter a numeric value for Amount.");
+
+        RuleFor(cru => cru.Week3_v)
+      .Cascade(CascadeMode.Continue)
+      .Must(IsAValidAmount).WithMessage("Please enter a numeric value for Amount.");
+
+        RuleFor(cru => cru.Week4_v)
+      .Cascade(CascadeMode.Continue)
+      .Must(IsAValidAmount).WithMessage("Please enter a numeric value for Amount.");
+
+        RuleFor(cru => cru.Week5_v)
+      .Cascade(CascadeMode.Continue)
+      .Must(IsAValidAmount).WithMessage("Please enter a numeric value for Amount.");
+
+
+    }
+
+    public bool IsValidYear(string date)
+    {
+        try
+        {
+            var year = Convert.ToDateTime(date).Year.ToString();
+            var regex = new Regex("^[0-9]+$");
+            return regex.IsMatch(year) && year.Length.Equals(4);
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public bool IsAValidMonth(string date)
+    {
+        try {
+            var month = Convert.ToDateTime(date).Month.ToString();
+            var regex = new Regex("(^0?[1-9]$)|(^1[0-2]$)");
+            return regex.IsMatch(month);
+        } catch(Exception ex) {
+            return false;
+        }
+
+    }
+
+    public bool IsAValidAmount(string amount)
+    {
+        var regex = new Regex("^-?\\d*(\\.\\d+)?$");
+
+        return regex.IsMatch(amount);
+    }
+}
+
